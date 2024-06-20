252882b11dc01de3aafa5595b626e511bcc7838a
sahilq312 <quraishisahil04@outlook.com>
2024-06-20 07:32:56 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,63 +16,22 @@ function App() {
   };
  */
   return (
-    <div className="w-full min-h-screen">
-        {/* <div className="flex flex-col items-center justify-center">
-          <div className="flex gap-6">
-            <h2>{questions.title}</h2>
-            <Link to={"/quiz"}>
-            <button>Start</button></Link>
-          </div>
-          {Quizzes?.title}
-          <Link to={"/create"}>
-          <button className="w-full" onClick={createQuiz}>Create Quiz</button></Link>
-        </div> */}
-        <div className="relative min-h-full isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-          <svg
-            viewBox="0 0 1024 1024"
-            className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
-            aria-hidden="true"
-          >
-            <circle cx={512} cy={512} r={512} fill="url(#759c1415-0410-454c-8f7c-9a820de03641)" fillOpacity="0.7" />
-            <defs>
-              <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
-                <stop stopColor="#7775D6" />
-                <stop offset={1} stopColor="#E935C1" />
-              </radialGradient>
-            </defs>
-          </svg>
-          <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-              Boost your productivity.
-              <br />
-              Start using our app today.
-            </h2>
-            <p className="mt-6 text-lg leading-8 text-gray-300">
-              Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+    <div className=" flex flex-col items-center justify-center w-full min-h-screen gap-10 text-3xl font-extrabold">
+      <div className=" leading-6 text-3xl font-bold">
+          Quiz App
+      </div>
+      <div className="flex items-center gap-10">
               <Link
                 to={"/quiz"}
-                className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+                className="rounded-md bg-white px-3.5 py-2.5 text-xl font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
               >
                 Start Quiz
               </Link>
-              <Link to={"/admin"} className="text-sm font-semibold leading-6 text-white">
+              <Link to={"/admin"} className="text-xl font-semibold leading-6 text-white">
                 Create Quiz <span aria-hidden="true">→</span>
               </Link>
-            </div>
-          </div>
-          <div className="relative mt-16 h-80 lg:mt-8">
-            <img
-              className="absolute left-0 top-0 w-[57rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
-              src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-              alt="App screenshot"
-              width={1824}
-              height={1080}
-            />
-          </div>
-        </div>
       </div>
+          </div>
     
   );
 }
--- src/lib/route.tsx
+++ src/lib/route.tsx
@@ -4,26 +4,18 @@ import App from "../App";
 import QuizApp from "../pages/quiz";
 
 const router = createBrowserRouter([
-    {
-        path : "/",
-        element : <App/>,
-        children : [
-            {
-                path : '/admin',
-                element : <Admin/>
-            },
-            {
-                path : '/',
-                element : <App/>
-                
-            }
-        ]
-    },
-    {
-        path : "/quiz",
-        element : <QuizApp/>
-    }
-    
-])
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
+  },
+  {
+    path : "/quiz",
+    element : <QuizApp/>
+  }
+]);
 
 export default router;
